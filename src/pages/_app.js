d296c866a346c615f84fc8fca5f959c82c726483
zadkiel <zadkiel.romero@pagodirecto.com>
2023-07-05 17:11:32 -0400

    [FOLLOW] next documentations for scripts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,8 +14,8 @@ export default function App({ Component, pageProps }) {
         <meta charSet="utf-8" name="viewport" content="width=device-width, initial-scale=1" />
         <title>React Shop</title>
       </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-1PY1FGVCFK" />
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-1PY1FGVCFK" />
+      <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
